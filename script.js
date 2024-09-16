8261ef8e1ccce4f5381df29e15a561183f7f1522
pini1258 <arte008@gmail.com>
2024-09-16 15:54:14 +0200

    Fixes

--- script.js
+++ script.js
@@ -527,6 +527,7 @@ function toggleSection(sectionId) {
     section.style.display = isVisible ? 'none' : 'block';
     localStorage.setItem(sectionId, isVisible ? 'hidden' : 'visible');
 
+    let visibility = isVisible ? 'hidden' : 'visible';
     const div = document.getElementById(sectionId + '_chart');
     if (div) {
         console.log(`Updating visibility for ${sectionId}_chart to ${visibility}`);
